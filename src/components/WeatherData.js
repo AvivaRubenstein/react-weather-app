0338093283c5897f00934c68ba184a13cc61c840
Aviva <avivadrubenstein@gmail.com>
2023-06-06 13:39:02 -0400

    added functionality to get one forecast from each day for the next 5 days, and only pass those objects from the original list array into the react component to display 5-day forecast

--- src/components/WeatherData.js
+++ src/components/WeatherData.js
@@ -1,22 +1,26 @@
 import React from 'react';
+require('../style.css')
 
-function Day({result}) {
-  let data;
+//trims the data from the API to only include today and next 5 days
+//maps through each of the days in the 5-day forecast and displays relevant weather data onto the page
+function Days({result}) {
+  let data = [];
   console.log(result)
   if(result.list.length){
-    data = result.list.slice(0, 6);
+    let array = 6;
+    for(let i=0; i < 6 && array < result.list.length; i++){
+      data.push(result.list[array])
+      array += 8;
+    }
     console.log(data);
   }
   
-
   return (
    
   <div>
-     <h2>Hello</h2>
     {data && data.length > 0 && data.map((day, index) => (
   <div className="card dayBoxes">
-    <div className="card-body " id={"day-" + index}>
-      
+    <div className="card-body" id={"day-" + index}>
     <h3 id ={"today-date-" + index}>{day.dt_txt.split(" ")[0]}</h3> 
     <img id ={"today-image-" + index} alt="weather-icon" src={"https://openweathermap.org/img/w/" + day.weather[0].icon + ".png"}/>
     <h5 id ={"today-temp-" + index}>Temperature: {day.main.temp} degrees</h5>
@@ -29,6 +33,7 @@ function Day({result}) {
     
   );
 }
+//function to display container of 5-day forecast data
 function WeatherData({data}) {
   
   return (
@@ -36,7 +41,7 @@ function WeatherData({data}) {
         <div className="col">
 
           <h3>5 Day Forecast:</h3>
-  <Day result={data}/>
+  <Days result={data}/>
           </div>
           </div>
 
